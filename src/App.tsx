dbc2098d1e400f2a66e3c0cfb50012f5757123f0
sdrjseka9 <sdrjseka9@naver.com>
2023-02-23 14:27:44 +0900

    0223

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
+import Subjects from './components/Subjects';
+import Header from './components/Header';
+import Lists from './components/Lists';
+import { useState } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import AddSubject from './components/AddSubject';
+import AddFunction from './components/AddFunction';
 
 function App() {
+  const [ subject, setSubject ] = useState(0);
+  const onChange = (subject:number) => {
+    setSubject(subject);
+  }
   return (
+    <BrowserRouter>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header sitename='typescript' onChange={onChange}/>
+      <Routes>
+        <Route path='/' element={<>
+          <Subjects onChange={onChange}/>
+          <Lists subject={subject}/>
+        </>}/>
+        <Route path='/addSubject' element={<AddSubject />}/>
+        <Route path='/addFunction' element={<AddFunction />}/>
+      </Routes>
     </div>
+    </BrowserRouter>
   );
 }
 
